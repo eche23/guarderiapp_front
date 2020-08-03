6915398eef9b3b8df24ea68e5e1aaff4a9f707b4
Echedey Garcias <eche2312@gmail.com>
2020-08-03 17:54:23 +0100

    create responsive menu

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,110 @@
-git<template>
+<template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <input type="checkbox" id="btn-menu" />
+      <label for="btn-menu" id="border-menu"><menu-icon id="icon-menu"/></label>
+      <div class="menu">
+        <img id="logo" alt="Vue logo" src="./assets/logo-baby.png" />
+        <router-link to="/" class="menu-item">Entradas/Salidas</router-link>
+        <router-link to="/about" class="menu-item">Base de Datos</router-link>
+        <router-link to="/about" class="menu-item">Facturación</router-link>
+        <router-link to="/about" class="menu-item">Gastos</router-link>
+        <router-link to="/about" class="menu-item">Notificaciones</router-link>
+        <router-link to="/about" class="menu-item">Agendas</router-link>
+        <router-link to="/about" class="menu-item">Cerrar Sesión</router-link>
+      </div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
+html {
+  height: 100%;
+}
+body {
+  margin: 0;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-wrap: wrap;
+  height: 100%;
+}
+
+#btn-menu {
+  display: none;
+}
+
+#icon-menu {
+  display: none;
 }
 
 #nav {
-  padding: 30px;
+  padding: 10px;
+  display: flex;
+  background: #79f29a;
+  justify-content: center;
+}
+
+.menu {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #ffffff;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #a64244;
+}
+
+.menu-item {
+  text-decoration: none;
+  font-size: 20px;
+}
+
+@media (max-width: 768px) {
+  #icon-menu {
+    display: block;
+    color: #ffffff;
+    cursor: pointer;
+    border: #ffffff solid 1px;
+    border-radius: 6px;
+    padding: 5px;
+    margin: 5px;
+  }
+
+  #border-menu {
+    max-width: 46px;
+  }
+
+  #nav {
+    height: 100%;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+  }
+
+  .menu {
+    display: none;
+  }
+
+  #btn-menu:checked ~ .menu {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 100%;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+}
+@media (max-width: 768px) {
+  .home {
+    margin-left: 65px;
+  }
+}</style
+>>
